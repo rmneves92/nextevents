e21d109b6475d8963393be04521f38e78b197266
rafael <rafael@IT-Admins-MacBook-Pro.local>
2021-05-22 12:19:27 -0300

    add search events form and layout

--- components/ui/button.js
+++ components/ui/button.js
@@ -1,11 +1,19 @@
 import Link from 'next/link';
 import classes from './button.module.css';
 
-function Button({ children, link }) {
+function Button(props) {
+  if (props.link) {
+    return (
+      <Link href={props.link}>
+        <a className={classes.btn}>{props.children}</a>
+      </Link>
+    );
+  }
+
   return (
-    <Link href={link}>
-      <a className={classes.btn}>{children}</a>
-    </Link>
+    <button className={classes.btn} onClick={props.onClick}>
+      {props.children}
+    </button>
   );
 }
 
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,8 +1,15 @@
+import { getAllEvents } from '../../dummy-data';
+import EventList from '../../components/events/event-list';
+import EventsSearch from '../../components/events/events-search';
+
 function AllEventsPage() {
+  const events = getAllEvents();
+
   return (
-    <div>
-      <h1>All Events</h1>
-    </div>
+    <>
+      <EventsSearch />
+      <EventList items={events} />
+    </>
   );
 }
 
