f16592482b97d37ab5cab291051a93f258b4998e
Rafael Neves <rafa.mneves@hotmail.com>
2021-06-21 14:02:58 -0300

    insert event comments in mongodb

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -10,12 +10,12 @@ async function handler(req, res) {
     }
 
     const client = await MongoClient.connect(
-      'mongodb+srv://rmneves:n6iIB25VXjLs3vQV@cluster0.kukjf.mongodb.net/newsletter?retryWrites=true&w=majority'
+      'mongodb+srv://rmneves:n6iIB25VXjLs3vQV@cluster0.kukjf.mongodb.net/events?retryWrites=true&w=majority'
     );
 
     const db = client.db();
 
-    await db.collection('emails').insertOne({ email: userEmail });
+    await db.collection('newsletter').insertOne({ email: userEmail });
 
     client.close();
 
